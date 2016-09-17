ac3a51bcd1dc5ae4fbbad3a628c6c5607cc39725
takanorip <takanori0170@gmail.com>
2016-09-17 13:34:38 +0900

    stage-0追加

--- sample_webpack/src/App.js
+++ sample_webpack/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import Drawer from './Drawer';
 
 injectTapEventPlugin();
 
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return(
       <MuiThemeProvider>
-        <MyAwesomeReactComponent />
+        <Drawer />
       </MuiThemeProvider>
     )
   }
